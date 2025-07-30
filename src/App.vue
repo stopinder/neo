dcfe0229004ac56a94f39e018b5bcdc19fef0748
stopinder <chrysalisifs@robormiston.com>
2025-07-30 18:59:39 +0700

    Finalize login check and Outseta fallback

--- src/App.vue
+++ src/App.vue
@@ -5,24 +5,27 @@
 
 <script setup>
 import { ref, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
 import LoginModal from './components/LoginModal.vue'
 
 const showLogin = ref(false)
+const route = useRoute()
 
 onMounted(() => {
-  if (window.Outseta && typeof window.Outseta.getCurrentUser === 'function') {
-    window.Outseta.getCurrentUser()
-        .then(user => {
-          localStorage.setItem('user', JSON.stringify(user))
-        })
-        .catch(() => {
-          localStorage.removeItem('user')
-          showLogin.value = true
-        })
-  } else {
-    console.warn('Outseta script not loaded.')
-    showLogin.value = true
+  if (route.path === '/full-report') {
+    if (window.Outseta && typeof window.Outseta.getCurrentUser === 'function') {
+      window.Outseta.getCurrentUser()
+          .then(user => {
+            localStorage.setItem('user', JSON.stringify(user))
+          })
+          .catch(() => {
+            localStorage.removeItem('user')
+            showLogin.value = true
+          })
+    } else {
+      console.warn('Outseta script not loaded.')
+      showLogin.value = true
+    }
   }
 })
 </script>
-
