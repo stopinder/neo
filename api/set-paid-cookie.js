6290cd3fb3a41a48112f136f95f715fc7b812395
stopinder <chrysalisifs@robormiston.com>
2025-08-02 09:41:06 +0700

    Configure set-paid-cookie as serverless function

--- api/set-paid-cookie.js
+++ api/set-paid-cookie.js
@@ -1,11 +1,10 @@
-// pages/api/set-paid-cookie.js
+// api/set-paid-cookie.js
 export default function handler(req, res) {
     const { redirect } = req.query;
-
-    // This sets a secure cookie that lasts 1 day
-    res.setHeader('Set-Cookie', 'hasPaid=true; Path=/; HttpOnly; Secure; SameSite=Strict; Max-Age=86400');
-
-    // This redirects the user to the page you want (usually /full-report)
+    res.setHeader(
+        'Set-Cookie',
+        'hasPaid=true; Path=/; HttpOnly; Secure; SameSite=Strict; Max-Age=86400'
+    );
     res.writeHead(302, { Location: redirect || '/' });
     res.end();
 }
